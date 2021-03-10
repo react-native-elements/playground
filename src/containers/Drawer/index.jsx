2299fce058fa7a4aa5eda38315cac69492591e3f
Ankush Dutt <ankushd100@gmail.com>
2021-03-10 19:27:41 +0530

    fix: Ability to close the navigation drawer in mobile on clicking outside

--- src/containers/Drawer/index.jsx
+++ src/containers/Drawer/index.jsx
@@ -65,12 +65,8 @@ function ResponsiveDrawer(props) {
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = (value) => {
-    if (value !== null) {
-      setMobileOpen(value);
-    } else {
-      setMobileOpen(!mobileOpen);
-    }
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
   const drawer = (
