77a45cd76662420b668298711be0823de58ea0e3
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-13 02:12:10 +0530

    feat🚀: update props explorer for airbnbRating

--- src/content/AirbnbRating/index.jsx
+++ src/content/AirbnbRating/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./searchbar.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./airbnb.mdx"));
 
 export default function AirbnbPlayground() {
   return (
     <div>
       <Playground />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PropDrawer>
+            <Content />
+          </PropDrawer>
+        </Suspense>
     </div>
   );
 }
