0675e197b47b0527272b0a98476b1c688d48eed9
Devesh <rockdev1234560@gmail.com>
2021-03-12 09:15:52 +0530

    mdx imports fixed

--- src/content/Avatar/index.jsx
+++ src/content/Avatar/index.jsx
@@ -1,6 +1,6 @@
 import React, { lazy, Component, Suspense } from "react";
 import Playground from "./avatar.playground.jsx";
-import importMDX from "mdx.macro";
+import {importMDX} from "mdx.macro";
 import PropDrawer from "../../components/PropDrawer";
 
 const Content = lazy(() => importMDX("./avatar.mdx"));
