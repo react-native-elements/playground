92f71134132387f6220b2089eb5f83a0e0e2c143
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 10:54:34 +0530

    feat: add to checkbox  prop_explorer

--- src/content/CheckBox/index.jsx
+++ src/content/CheckBox/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./checkbox.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./checkbox.mdx"));
 
 export default function CheckBoxPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
