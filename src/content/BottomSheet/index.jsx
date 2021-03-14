815bf0e38e9fe57528de3dcd8638af6e0ce6370c
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:17:12 +0530

    feat: added propExplorer in buttomSheet

--- src/content/BottomSheet/index.jsx
+++ src/content/BottomSheet/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import Playground from "./bottomsheet.playground.jsx";
+import React  , { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+import Playground from "./bottomsheet.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./bottomsheet.md"));
 export default function BottomSheetPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
