6cc1d008eb07336636f338b08781f26dcaa68f94
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 12:04:10 +0530

    propExplorer added to SerchBar

--- src/content/SearchBar/index.jsx
+++ src/content/SearchBar/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Playground from "./searchbar.playground.jsx";
+import React  ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./searchbar.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./searchbar.mdx"));
 export default function SearchBarPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
