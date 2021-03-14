d5e1c2dba8b90314f7d820a49d002fdea7a1cf6c
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:24:50 +0530

    feat: added propExplorer to divider

--- src/content/Divider/index.jsx
+++ src/content/Divider/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Playground from "./divider.playground.jsx";
+import React , { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./divider.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./divider.mdx"));
 export default function DividerPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
