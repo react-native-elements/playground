281f9670355cb9f9048dda27b832e31699971b57
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:40:35 +0530

    feat: added propExplorer to image

--- src/content/Icon/index.jsx
+++ src/content/Icon/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import Playground from "./icon.playground.jsx";
+import React ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./icon.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+
+const Content = lazy(() => importMDX("./icon.mdx"));
 export default function IconPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
