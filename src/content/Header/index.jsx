16cc09b810ff2c635b130e4ad7d8941e0a823e69
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:30:46 +0530

    feat: added propExplorer to header

--- src/content/Header/index.jsx
+++ src/content/Header/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Playground from "./header.playground.jsx";
+import React , { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./header.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./header.mdx"));
 export default function HeaderPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
