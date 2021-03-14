1b9eef8faa5fe7ce2a52130a145c74f5b1b06aca
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 10:47:53 +0530

    feat: Updated bottomsheet and buttongroup prop_explorer

--- src/content/BottomSheet/index.jsx
+++ src/content/BottomSheet/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./bottomsheet.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./bottomsheet.mdx"));
 
 export default function BottomSheetPlayground() {
   return (
     <div>
       <Playground />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PropDrawer>
+            <Content />
+          </PropDrawer>
+        </Suspense>
     </div>
   );
 }
--- src/content/ButtonGroup/index.jsx
+++ src/content/ButtonGroup/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./buttongroup.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./buttongroup.mdx"));
 
 export default function ButtonGroupPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
