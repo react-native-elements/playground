b9dd1f2a6029d4168f33eb2db4aee62cad39dca1
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 12:13:25 +0530

    propExplorer added to text

--- src/content/SocialIcon/index.jsx
+++ src/content/SocialIcon/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Playground from "./socialicon.playground.jsx";
+import React  ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./socialicon.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./socialcon.mdx"));
 export default function SocialIconPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense>
+      <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
--- src/content/Text/index.jsx
+++ src/content/Text/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import Playground from "./tooltip.playground.jsx";
+import React  ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./tooltip.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+
+const Content = lazy(() => importMDX("./text.mdx"));
 export default function TextPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
