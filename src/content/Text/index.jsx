9ab75d96dafb5dd4c9cb190a3b6d61478df640cb
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-15 00:02:28 +0530

    feat: add to text prop explorer

--- src/content/Text/index.jsx
+++ src/content/Text/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./tooltip.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./text.mdx"));
+
 
 export default function TextPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
