2e609ecbd8e77195c993b6f95c83ae98b807594e
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 11:54:27 +0530

    feat: add to icon prop_explorer

--- src/content/Icon/index.jsx
+++ src/content/Icon/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./icon.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./icon.mdx"));
 
 export default function IconPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
