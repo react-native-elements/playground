82dff5a200830af1f20a4068210dbe1f2efebcfa
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:47:16 +0530

    input /index.jsx updated

--- src/content/Image/index.jsx
+++ src/content/Image/index.jsx
@@ -2,9 +2,10 @@ import React ,  { lazy, Suspense } from "react";
 import { importMDX } from "mdx.macro";
 
 
-import Playground from "./icon.playground.jsx";
+import Playground from "./input.playground.jsx";
 import PropDrawer from "../../components/PropDrawer";
 
+const Content = lazy(() => importMDX("./image.mdx"));
 export default function ImagePlayground() {
   return (
     <div>
