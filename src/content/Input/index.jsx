e1aa25b810018bde461f118ae7be5b3ea80e00da
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:45:38 +0530

    feat: added propExplorer to input

--- src/content/Input/index.jsx
+++ src/content/Input/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Playground from "./input.playground.jsx";
+import React ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./icon.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./input.mdx"));
 export default function InputPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
