42c54bc4690539d3dbfd4e28a72eb2f183decff5
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 12:18:57 +0530

    propExplorer added to tooltip

--- src/content/Tile/index.jsx
+++ src/content/Tile/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import Playground from "./tile.playground.jsx";
+import React  ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+import Playground from "./tile.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./title.mdx"));
 export default function TilePlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 };
--- src/content/ToolTip/index.jsx
+++ src/content/ToolTip/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import Playground from "./tooltip.playground.jsx";
+import React  ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
+
+import Playground from "./tooltip.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
+
+
+const Content = lazy(() => importMDX("./tooltip.mdx"));
 export default function ToolTipPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
