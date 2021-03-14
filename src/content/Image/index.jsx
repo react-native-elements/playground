18a31f2bb49daeccb32cf97a40eedcd72a59ede2
Gaurav Tewari <gauravtewari111@gmail.com>
2021-03-14 11:42:26 +0530

    feat: added propExplore to image

--- src/content/Image/index.jsx
+++ src/content/Image/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import Playground from "./input.playground.jsx";
+import React ,  { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
+
+
+import Playground from "./icon.playground.jsx";
+import PropDrawer from "../../components/PropDrawer";
 
 export default function ImagePlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
