0195bcce9eaf2318d7b0880428d49cb40d627e01
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-15 00:06:25 +0530

    feat: add to tooltip prop explorer

--- src/content/ToolTip/index.jsx
+++ src/content/ToolTip/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./tooltip.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./tooltip.mdx"));
 
 export default function ToolTipPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
