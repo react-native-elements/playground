6d7ebb5767ce39986118450cdbc7566b4cb23774
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 12:04:01 +0530

    feat: add to image prop_explorer

--- src/content/Image/index.jsx
+++ src/content/Image/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./input.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./image.mdx"));
 
 export default function ImagePlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
