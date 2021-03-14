212474fa3c169934aacafa16a8d046583133573b
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-15 00:25:54 +0530

    feat: add to pricing prop explorer

--- src/content/Pricing/index.jsx
+++ src/content/Pricing/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./pricing.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./pricing.mdx"));
 
 export default function PricingPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
