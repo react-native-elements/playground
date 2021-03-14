71bdf72990ead6744a7a9f01ea756912a73eb955
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-15 00:00:32 +0530

    feat: add to social_icon prop explorer

--- src/content/SocialIcon/index.jsx
+++ src/content/SocialIcon/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./socialicon.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./socialicon.mdx"));
 
 export default function SocialIconPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
