9243a8ecd6c3a4fed20443d54d9614b4f57e7253
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 10:58:43 +0530

    feat: add to card  prop_explorer

--- src/content/Card/index.jsx
+++ src/content/Card/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./card.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./card.mdx"));
 
 export default function CardPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
