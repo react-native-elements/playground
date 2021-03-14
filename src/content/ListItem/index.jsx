2b2d268aba39eea2226240192883c576d6198f61
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-14 12:20:58 +0530

    feat: add to listItem prop_explorer

--- src/content/ListItem/index.jsx
+++ src/content/ListItem/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./listitem.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("./listitem.mdx"));
 
 export default function ListItemPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
