06d8b5241204d5fbdb7a0f7431915b90f4ae4d34
Pranshu Chittora <pranshuchittora17@gmail.com>
2021-03-20 02:58:12 +0530

    feat: Remote docs (#15)
    * feat: Remote docs
    
    * feat: Map all components to remote props
    
    * feat: Added Netlify.toml
    
    * chore: Modified Netlify config
    
    * feat: Added Netlify x GitHub Actions Pipeline
    
    * fix: CI
    
    * feat: SVN -> Git
    
    * feat: Added netlify.toml
    
    * fix: CI Fix
    
    * fix: Netlify config
    
    * fix: CI
    
    * fix: Get rid of netlify.toml

--- src/content/ListItem/index.jsx
+++ src/content/ListItem/index.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
+
+import PropDrawer from "../../components/PropDrawer";
+
 import Playground from "./listitem.playground.jsx";
+
+const Content = lazy(() => importMDX("../Props/listitem.md"));
 
 export default function ListItemPlayground() {
   return (
--- src/content/Overlay/index.jsx
+++ src/content/Overlay/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Playground from "./overlay.playground.jsx";
+import { importMDX } from "mdx.macro";
+import PropDrawer from "../../components/PropDrawer";
+
+const Content = lazy(() => importMDX("../Props/overlay.md"));
 
 export default function OverlayPlayground() {
   return (
     <div>
       <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
     </div>
   );
 }
