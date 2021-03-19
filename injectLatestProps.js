61ae5460a9bdb0e089e2f365f68cf506929ca65c
Pranshu Chittora <pranshuchittora17@gmail.com>
2021-03-19 22:47:26 +0530

    feat: SVN -> Git

--- injectLatestProps.js
+++ injectLatestProps.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 exec(
-  "svn export https://github.com/react-native-elements/react-native-elements/trunk/website/docs/props ./src/content/Props",
+  "git clone https://github.com/react-native-elements/react-native-elements --depth=1",
   (err, stdout, stderr) => {
     if (err) {
       // node couldn't execute the command
@@ -12,3 +12,14 @@ exec(
     console.log(`stderr: ${stderr}`);
   }
 );
+exec("cp -r ./react-native-elements/website/docs/props ./src/content/Props"),
+  (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  };
