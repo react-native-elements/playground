8c6c979415433569063849d27087c81807af3401
Ankush Dutt <51174480+ankushdutt@users.noreply.github.com>
2021-03-26 23:48:17 +0530

    onPress does not select button and updates selectedIndex (#89)

--- src/content/ButtonGroup/buttongroup.playground.jsx
+++ src/content/ButtonGroup/buttongroup.playground.jsx
@@ -53,7 +53,11 @@ const ButtonGroupPlayground = () => {
       },
       onPress: {
         type: PropTypes.Function,
-        value: `() => console.log("onPress()")`,
+        value: `(selectedIdx) => setSelectedIndex(selectedIdx)`,
+        propHook: {
+          what: `selectedIdx`,
+          into: `selectedIndex`,
+        }
       },
       selectMultiple: {
         type: PropTypes.Boolean,
@@ -66,10 +70,12 @@ const ButtonGroupPlayground = () => {
       selectedIndex: {
         type: PropTypes.Number,
         value: `1`,
+        stateful: true
       },
       selectedIndexes: {
         type: PropTypes.Array,
-        value: `[1,2]`,
+        value: `[]`,
+        stateful: true
       },
       selectedTextStyle: {
         type: PropTypes.Object,
