b32f020e75e054ec2b553d2bdd76c477645f2ca5
Uyadav207 <yadavutkarsh207@gmail.com>
2021-03-27 14:43:17 +0530

    code-cleaning minor warnings removal

--- src/content/Icon/icon.playground.jsx
+++ src/content/Icon/icon.playground.jsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { useState } from "react";
-import { View } from "react-native";
 import { Icon } from "react-native-elements";
 
 import Playground from "../../components/playground";
