985b20eae2d7c7ffbede4df9b5a92f056d627c80
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2021-04-21 14:20:05 +0530

    chore: Add Speedial Playground

---
+++ src/content/SpeedDial/index.jsx
@@ -0,0 +1,20 @@
+import React, { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
+
+import PropDrawer from "../../components/PropDrawer";
+import Playground from "./speeddial.playground";
+
+const Content = lazy(() => importMDX("../Props/speeddial.md"));
+
+export default function SpeedDialPlayground() {
+  return (
+    <div>
+      <Playground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PropDrawer>
+          <Content />
+        </PropDrawer>
+      </Suspense>
+    </div>
+  );
+}
