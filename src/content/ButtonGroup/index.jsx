4be8346615a47cca0b4eb39b763b09d901e8932d
Shaurya Singhal <shauryasinghal84@gmail.com>
2021-12-23 20:21:22 +0530

    chore: add Loading Component in fallbacks (#103)

--- src/content/ButtonGroup/index.jsx
+++ src/content/ButtonGroup/index.jsx
@@ -2,15 +2,16 @@ import React, { lazy, Suspense } from "react";
 import { importMDX } from "mdx.macro";
 
 import PropDrawer from "../../components/PropDrawer";
-
+import Spinner from "../../containers/Spinner";
 import Playground from "./buttongroup.playground.jsx";
+
 const Content = lazy(() => importMDX("../Props/button_group.md"));
 
 export default function ButtonGroupPlayground() {
   return (
     <div>
       <Playground />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Spinner />}>
         <PropDrawer>
           <Content />
         </PropDrawer>
--- src/content/Icon/index.jsx
+++ src/content/Icon/index.jsx
@@ -2,15 +2,16 @@ import React, { lazy, Suspense } from "react";
 import { importMDX } from "mdx.macro";
 
 import PropDrawer from "../../components/PropDrawer";
-
+import Spinner from "../../containers/Spinner";
 import Playground from "./icon.playground.jsx";
+
 const Content = lazy(() => importMDX("../Props/icon.md"));
 
 export default function IconPlayground() {
   return (
     <div>
       <Playground />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Spinner />}>
         <PropDrawer>
           <Content />
         </PropDrawer>
